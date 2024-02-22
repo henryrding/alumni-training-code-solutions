415c0aeaa786f32dd4e039462c33477b71fb5e10
Henry Ding <dinghenryr@gmail.com>
2024-02-22 04:30:29 +0000

    Completed typescript-primitives-and-variables exercise.

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'Henry Ding';
+const isCool = true;
+const totalPets = 0;
+const unknown = null;
+const never = undefined;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+console.log('value of isCool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,20 @@
+const fullName: string = 'Henry Ding';
+const isCool: boolean = true;
+const totalPets: number = 0;
+const unknown: null = null;
+const never: undefined = undefined;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+console.log('value of isCool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+console.log('value of totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
